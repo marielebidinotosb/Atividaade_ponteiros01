990b831a276ec219758670829b84399806c2eb8e
Mariele Bidinoto <marielebidinotosb@gmail.com>
2026-08-26 09:26:25 -0300

    Implement pointer usage in exercicio_01.c

--- exercicio_01.c
+++ exercicio_01.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main(void){
+    int x = 20;
+    int *p = &x;
+    
+    printf ("Valor de X: %d\n", x);
+    printf ("Endereço de X: %p\n", (void*)&x);
+    printf ("Conteúdo de P (Endereço guardado): %p\n", (void*)p);
+    printf ("Valor apontado por P (*p): %d\n", *p);
+
+    //Alterado o valor de x para 35 via ponteiro 
+    *p = 35;
+    printf ("\nNovo valor de X: %d\n", x);
+
+    return 0;
+    
+}ss
